687050c67fdcf947178aeb857bbed48809ab489b
Santhu-12 <manikantasanthu12@gmail.com>
2024-10-13 09:56:12 +0530

    import issue fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Register from "./components/auth/register";
+import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Dashboard from "./components/dashboard/Dashboard";
 
